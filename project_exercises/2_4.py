397e0e7c10d8e04efc3a70c2cd91021cd5bef4f4
Roberto Bueno <roberto.buenodelacruz@gmail.com>
2024-01-10 09:01:07 -0600

    working on 2.4

---
+++ project_exercises/2_4.py
@@ -0,0 +1,9 @@
+print("Price Comparison")
+
+price_of_64 = float(input("Price of 64 oz size: "))
+price_of_32 = float(input)("Price of 32 oz size: ")
+
+oz = float(input("How many oz are in the container?"))
+
+price_per_oz = price_of_32 / oz
+
